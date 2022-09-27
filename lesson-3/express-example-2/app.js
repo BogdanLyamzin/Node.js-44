fcc346836f4ca0262c12a40a05dfb20fb154b7a3
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-09-27 21:15:19 +0300

    finish lesson 3

---
+++ lesson-3/express-example-2/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const books = require("./books");
+
+const app = express();
+
+app.set("json spaces", 8)
+
+app.get("/books", (req, res)=> {
+    const databaseResponse = null;
+    // res.json(databaseResponse)
+    // res.send(databaseResponse)
+    res.json(books)
+    // res.send(books)
+});
+
+app.listen(3000);
